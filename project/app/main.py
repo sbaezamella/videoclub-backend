97132e9af7b9a441f1530d994f5b15541bf3e5b7
Sebastian Baeza <sbaezamella@gmail.com>
2021-05-06 17:00:13 -0400

    :recycle: Refactor main and ping route

--- project/app/main.py
+++ project/app/main.py
@@ -1,22 +1,37 @@
+import logging
+
 from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
 
 from app.api.v1.api import api_router
+from app.api.v1.endpoints.ping import router
+from app.db.init import init_db
+from app.core.config import settings
 
-app = FastAPI()
-
-register_tortoise(
-    app,
-    db_url="postgres://postgres:password@db/videoclub",
-    modules={"models": ["app.models.genres"]},
-    generate_schemas=True,
-    add_exception_handlers=True,
-)
+log = logging.getLogger(__name__)
 
 
-@app.get("/ping")
-async def ping():
-    return {"ping": "pong"}
+def create_application() -> FastAPI:
+    application = FastAPI(
+        title=settings.PROJECT_NAME,
+        description="Videoclub RESTful API",
+        version="1.0",
+        openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    )
+    application.include_router(api_router, prefix="/api/v1")
+    application.include_router(router)
+
+    return application
 
 
-app.include_router(api_router, prefix="/api/v1")
+app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
