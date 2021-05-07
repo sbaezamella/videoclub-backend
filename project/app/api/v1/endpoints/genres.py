1a084d34c2e43b6c93ac2e10f4a09611ce213ad8
Sebastian Baeza <sbaezamella@gmail.com>
2021-05-07 01:32:03 -0400

    :sparkles: Add the rest of genre CRUD routes

--- project/app/api/v1/endpoints/genres.py
+++ project/app/api/v1/endpoints/genres.py
@@ -1,12 +1,64 @@
 from typing import List
 
-from fastapi import APIRouter
-
 from app import models, schemas
+from app.crud import crud_genre
+from fastapi import APIRouter, HTTPException, Path, Response, status
+from tortoise.exceptions import IntegrityError
 
 router = APIRouter()
 
 
 @router.get("/", response_model=List[schemas.Genre])
-async def get_genres():
-    return await models.Genre.all()
+async def get_all_genres() -> List[schemas.Genre]:
+    genres = await crud_genre.get_all()
+    return genres
+
+
+@router.post(
+    "/", response_model=schemas.Genre, status_code=status.HTTP_201_CREATED
+)  # noqa
+async def create_genre(payload: schemas.GenreCreate) -> models.Genre:
+    try:
+        new_genre = await crud_genre.post(payload)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Genre already exists",
+        )
+    return new_genre
+
+
+@router.get("/{id}", response_model=schemas.Genre)
+async def get_genre(id: int = Path(..., ge=1)):
+    genre = await crud_genre.get(id)
+    if not genre:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Genre not found",
+        )
+    return genre
+
+
+@router.put("/{id}", response_model=schemas.Genre)
+async def update_genre(payload: schemas.GenreUpdate, id: int = Path(..., ge=1)):
+    # TODO: validate if name is taken
+    genre = await crud_genre.put(id, payload)
+
+    if not genre:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Genre not found",
+        )
+
+    return genre
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_genre(id: int = Path(..., ge=1)):
+    genre = await crud_genre.delete(id)
+    if not genre:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Genre not found",
+        )
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
---
+++ project/app/crud/crud_genre.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import List, Union
+
+from app.models import Genre
+from app.schemas import GenreCreate, GenreUpdate
+
+
+async def get_all() -> List:
+    genres = await Genre.all().values()
+    return genres
+
+
+async def post(payload: GenreCreate) -> Genre:
+    datetime_now = datetime.now()
+    new_genre = await Genre.create(
+        **payload.dict(),
+        created_at=datetime_now,
+        updated_at=datetime_now,
+    )
+    return new_genre
+
+
+async def get(id: int) -> Union[dict, None]:
+    genre = await Genre.filter(id=id).first().values()
+    if genre:
+        return genre[0]
+    return None
+
+
+async def put(id: int, payload: GenreUpdate) -> Union[dict, None]:
+    # TODO: validate if name is taken
+    genre = await Genre.filter(id=id).update(**payload.dict())
+    if genre:
+        updated_genre = await Genre.filter(id=id).first().values()
+        return updated_genre[0]
+    return None
+
+
+async def delete(id: int):
+    genre = await Genre.filter(id=id).delete()
+    print(genre)
+    return genre
