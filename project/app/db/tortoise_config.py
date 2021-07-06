15e488eca74c1a90dc4b4b06628a087d0d40bc82
Sebastian Baeza <sbaezamella@pm.me>
2021-07-05 21:24:31 -0400

    :sparkles: Add aerich migration tool

--- project/app/db/tortoise_config.py
+++ project/app/db/tortoise_config.py
@@ -1,15 +1,6 @@
-import logging
-
-from fastapi import FastAPI
-from tortoise import Tortoise, run_async
-from tortoise.contrib.fastapi import register_tortoise
-
 import os
 
-
-log = logging.getLogger(__name__)
-
-config = {
+TORTOISE_ORM = {
     "connections": {
         "default": {
             "engine": "tortoise.backends.asyncpg",
@@ -27,30 +18,9 @@ config = {
             "models": [
                 "app.models.genres",
                 "app.models.movies",
+                "aerich.models",
             ],
             "default_connection": "default",
         },
     },
 }
-
-
-def init_db(app: FastAPI) -> None:
-    register_tortoise(
-        app,
-        config=config,
-        add_exception_handlers=True,
-    )
-
-
-async def generate_schema() -> None:
-    log.info("Initializing Tortoise...")
-
-    await Tortoise.init(
-        config=config,
-    )
-    log.info("Generating database schema via Tortoise...")
-    await Tortoise.generate_schemas()
-
-
-if __name__ == "__main__":
-    run_async(generate_schema())
